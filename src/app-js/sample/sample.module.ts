83dd6414c730e6dcf0a90bce55560be2f0edc9eb
wangjun <wangjun1yang@gmail.com>
2020-03-22 23:33:48 +0200

    make angular.js and angular both running

--- src/app-js/sample/sample.module.ts
+++ src/app-js/sample/sample.module.ts
@@ -1,25 +1,18 @@
 import * as angular from 'angular';
 angular.module('MyApp', [])
     .controller('MyCtrl', function() {
-        this.title = 'New component in AngularJS 1.5!';
-        this.body = 'A nice way to write components';
+        this.test = {
+            detail: 'This is from angular.js, both angularJs and angular are running'
+        };
     })
     .component('panel', {
         bindings: {
-            title: '='
+            test: '='
         },
         template: [
-            '<div class="card" ng-click="panel.toggle()">',
-            '<div class="card-header" ng-click="panel.toggle()"> {{panel.title}} </div>',
-            '<div class="card-block" ng-class="{hide: !panel.opened}"></div>',
+            '<div class="card">',
+            '<div class="card-block" > {{$ctrl.test.detail }} </div>',
             '</div>'
         ].join(''),
-        controller: function() {
-            this.opened = false;
-            this.toggle = function () {
-                console.log('test');
-                this.opened = !this.opened;
-            };
-        }
     });
 
